6d76dff5f41d5b95430cbaf2816aeac1e8be5f50
Aryan <aryanghugare75@gmail.com>
2026-04-07 23:58:46 +0530

    Add logo SVG and initialize Prisma schema with user and project models

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function App() {
   return (
-    <div>This is for react</div>
+    <div className='bg-gray-500 p-4 rounded-lg shadow-md text-amber-200'  >This is for react</div>
   )
 }
 
