ed07a8493235be48e185f908e1626077315812a0
Aryan <aryanghugare75@gmail.com>
2026-07-31 13:45:07 +0530

    implement project management features including project creation, version rollback, and code saving

--- server/routes/userRoutes.ts
+++ server/routes/userRoutes.ts
@@ -1,11 +1,16 @@
 import express from 'express';
 import { protect } from '../middlewares/auth.js';
-import { getUserCredits } from '../controllers/userController.js';
+import { createUserProject, getUserCredits, getUserProject, getUserProjects, purchaseCredits, togglePublish } from '../controllers/userController.js';
 
 
 const userRouter = express.Router();
 
-userRouter.get('/credits',protect, getUserCredits) ;
+userRouter.get('/credits',protect, getUserCredits)
+userRouter.post('/project',protect, createUserProject)
+userRouter.get('/project/:projectId',protect, getUserProject)
+userRouter.get('/projects',protect, getUserProjects)
+userRouter.get('/publish-toggle/:projectId',protect, togglePublish)
+userRouter.post('/purchase-credits',protect, purchaseCredits)
 
 
 
--- server/server.ts
+++ server/server.ts
@@ -3,6 +3,7 @@ import express, { Request, Response } from 'express';
 import cors from "cors";
 import {toNodeHandler} from 'better-auth/node'
 import { auth } from "./lib/auth.js";
+import userRouter from "./routes/userRoutes.js";
 const app = express();
 
 const corsOptions = {
@@ -18,10 +19,13 @@ app.use(express.json());
 const port = process.env.PORT || 3000;
 
 app.all('/api/auth/{*any}', toNodeHandler(auth));
+app.use(express.json({limit : '50mb'}))
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Server is Live!');
 });
+
+app.use('/api/user', userRouter);
 
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
