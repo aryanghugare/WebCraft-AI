b5586a995af590fc2c6fb40d1cb2875ef16228e9
Aryan <aryanghugare75@gmail.com>
2026-08-01 18:38:30 +0530

    Preview component

--- client/src/pages/Preview.tsx
+++ client/src/pages/Preview.tsx
@@ -1,35 +1,45 @@
-import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom';
-import { dummyProjects } from '../assets/assets';
-import { Loader2Icon } from 'lucide-react';
-import ProjectPreview from '../components/ProjectPreview';
-import type { Project } from '../types';
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { Loader2Icon } from "lucide-react";
+import ProjectPreview from "../components/ProjectPreview";
+import type { Project, Version } from "../types";
+import api from "@/configs/axios";
+import { toast } from "sonner";
+import { authClient } from "@/lib/auth-client";
+
 
 const Preview = () => {
 
-const {projectId,versionId} = useParams() ;
-const [code,setCode] = useState("") ;
-const [loading , setLoading] = useState(true) ;
+  const {data: session, isPending} = authClient.useSession()
+  const { projectId, versionId } = useParams()
+  const [code, setCode] = useState('');
+  const [loading, setLoading] = useState(true);
 
-const fetchCode = async () => {
+  const fetchCode = async () => {
+   try {
+    const { data } = await api.get(`/api/project/preview/${projectId}`)
+    setCode(data.project.current_code)
+    if(versionId){
+      data.project.versions.forEach((version: Version)=>{
+        if(version.id === versionId){
+          setCode(version.code)
+        }
+      })
+    }
+    setLoading(false)
+   } catch (error: any) {
+    toast.error(error?.response?.data?.message || error.message);
+    console.log(error);
+   }
+  }
 
+  useEffect(()=>{
+    if(!isPending && session?.user){
+      fetchCode()
+    }
+  },[session?.user])
 
-setTimeout(()=>{
-const code = dummyProjects.find((project)=> project.id === projectId)?.current_code ;
-if(code) {
-setCode(code);
-setLoading(false) ;
-}
-},2000)
-}
-
-
-// fetching the code at mounting 
-useEffect(()=>{
-fetchCode() 
-},[])
-
- if(loading){
+  if(loading){
     return (
       <div className='flex items-center justify-center h-screen'>
         <Loader2Icon className='size-7 animate-spin text-indigo-200' />
@@ -37,7 +47,7 @@ fetchCode()
     )
   }
   return (
-   <div className="h-screen">
+    <div className="h-screen">
       {code && <ProjectPreview project={{current_code: code} as Project} isGenerating={false} showEditorPanel={false}/>}
     </div>
   )
