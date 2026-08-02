5a3423def47a9d9851bbd782bee63a3dd89f445e
Aryan <aryanghugare75@gmail.com>
2026-08-02 17:31:02 +0530

    Stripe Functionality

--- server/server.ts
+++ server/server.ts
@@ -5,6 +5,7 @@ import {toNodeHandler} from 'better-auth/node'
 import { auth } from "./lib/auth.js";
 import userRouter from "./routes/userRoutes.js";
 import projectRouter from "./routes/projectRoutes.js";
+import { stripeWebhook } from "./controllers/stripeWebHook.js";
 const app = express();
 
 const corsOptions = {
@@ -21,7 +22,7 @@ const port = process.env.PORT || 3000;
 
 app.all('/api/auth/{*any}', toNodeHandler(auth));
 app.use(express.json({limit : '50mb'}))
-
+app.post('/api/stripe', express.raw({type: 'application/json'}), stripeWebhook)
 app.get('/', (req: Request, res: Response) => {
     res.send('Server is Live!');
 });
